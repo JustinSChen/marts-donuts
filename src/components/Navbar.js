66de46514e40f0413485268e6fc449b45ef8ada5
Justin <justinsengchen@gmail.com>
2024-09-13 00:19:51 -0700

    progress

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,82 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import logo from '../assets/logo.svg';
+import React, { useState, useEffect } from "react";
+import logo from "../assets/logo.svg";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
+  const [isMobile, setIsMobile] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 600px)");
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    handleMediaQueryChange(mediaQuery);
+
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+
+  const openNav = () => {
+    setMenuOpen(true);
+  };
+
+  const closeNav = () => {
+    setMenuOpen(false);
+  };
+
   return (
-    <nav className="nav">
-      <div className="nav-container">
-        <div className="logo">
-          <img src={logo} alt="Donut Shop Logo" />
-        </div>
-        <div className="nav-text">
-          <p>+1 (760) 741-0717</p>
-        </div>
-      </div>
-      <div className="nav-links">
-        <a href="/">Home</a>
-        <a href="/#/menu">Menu</a>
-        <a href="/#/catering">Catering</a>
-        <a href="/#/about">About</a>
-        <a href="/#/contact">Contact Us</a>
-        <a href="/#/location">Location</a>
-      </div>
-    </nav>
+    <>
+      {isMobile ? (
+        <nav className="mobile nav">
+          <div className="icons">
+              { menuOpen 
+              ? 
+              <FontAwesomeIcon icon={faXmark} onClick={closeNav}/>
+              :
+              <FontAwesomeIcon icon={faBars} onClick={openNav}/>
+              }
+          </div>
+          <div className="logo">
+              <img src={logo} alt="Donut Shop Logo" />
+          </div>
+          <div className={`overlay ${menuOpen ? 'open' : ''
+          }`}>
+            {menuOpen ? 
+            <>
+            <a href="/#/menu">Menu</a>
+            <a href="/#/location">Location</a>
+            <a href="/#/about">Our story</a></>  : <></>
+            
+            }
+          </div>
+        </nav>
+      ) : (
+        <nav className="desktop nav">
+          <div className="nav-container">
+            <div className="logo">
+              <img src={logo} alt="Donut Shop Logo" />
+            </div>
+            <div className="nav-text">
+              <p>+1 (760) 741-0717</p>
+            </div>
+          </div>
+          <div className="nav-links">
+            <a href="/">Home</a>
+            <a href="/#/menu">Menu</a>
+            <a href="/#/catering">Catering</a>
+            <a href="/#/about">About</a>
+            <a href="/#/contact">Contact Us</a>
+            <a href="/#/location">Location</a>
+          </div>
+        </nav>
+      )}
+    </>
   );
 };
 
